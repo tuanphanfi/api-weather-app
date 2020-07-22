09572b309d1c60ffa30d147f2e0e0ed19f9e7f24
tuanphanfi <tuanphan.fi@gmail.com>
2020-07-22 17:56:45 +0700

    3rd attemp

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,21 +4,19 @@ export default class Nav extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Weather</a>
+                <button className="navbar-brand">Weather</button>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Feature <span className="sr-only">(current)</span></a>
+                            <button className="nav-link">Feature <span className="sr-only">(current)</span></button>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <button className="nav-link">Pricing</button>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
-                        </li>
+
                     </ul>
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" />
